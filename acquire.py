e4dceebdedc1a31eac2d22e1179aba38610aa4a6
Ryan McCall <ryan.e.mccall@gmail.com>
2020-04-08 10:24:55 -0500

    Add get_data()

--- acquire.py
+++ acquire.py
@@ -1,4 +1,14 @@
-
+import env
+import pandas as pd
 
 def get_url(database):
-    return f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    '''
+    database: string; name of database that the url is being created for
+    '''
+    return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{database}'
+
+def get_data():
+    zillow = pd.read_csv('zillow.csv')
+    FIPS = pd.read_csv('fips.csv')
+    zillow = zillow.merge(FIPS, left_on='fips', right_on='FIPS')
+    return zillow
