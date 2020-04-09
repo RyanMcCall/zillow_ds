fc561793243c1d053fbc6d13cc19f03a35f55b57
Ryan McCall <ryan.e.mccall@gmail.com>
2020-04-09 14:50:38 -0500

    Add more conversions to int and convert lat/long

--- prep.py
+++ prep.py
@@ -8,7 +8,16 @@ def acquire_and_prep_data():
     zillow = zillow.drop_duplicates()
     zillow = zillow.dropna()
     zillow = zillow.drop(columns='fips')
+
     zillow.bedroomcnt = zillow.bedroomcnt.astype('int')
+    zillow.calculatedfinishedsquarefeet = zillow.calculatedfinishedsquarefeet.astype('int')
+    zillow.fullbathcnt = zillow.fullbathcnt.astype('int')
+    zillow.yearbuilt = zillow.yearbuilt.astype('int')
+    zillow.taxvaluedollarcnt = zillow.taxvaluedollarcnt.astype('int')
+
     zillow = zillow.rename(columns={'calculatedfinishedsquarefeet': 'squarefeet', 'Name': 'County'})
+    
+    zillow.latitude = zillow.latitude / 1000000
+    zillow.longitude = zillow.longitude / 1000000
 
     return zillow
