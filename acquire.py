c0dea35f442bc18f8c72ac886789ecd0987314b2
Ryan McCall <ryan.e.mccall@gmail.com>
2020-04-09 11:41:23 -0500

    Change get_data to complete all retrieval automatically

--- acquire.py
+++ acquire.py
@@ -1,5 +1,6 @@
 import env
 import pandas as pd
+from os import path
 
 def get_url(database):
     '''
@@ -7,8 +8,34 @@ def get_url(database):
     '''
     return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{database}'
 
+def label_county(row):
+    if row['fips'] == 6037:
+        return 'Los Angeles'
+    elif row['fips'] == 6059:
+        return 'Orange'
+    elif row['fips'] == 6111:
+        return 'Ventura'
+
 def get_data():
+    if not path.isfile("zillow.csv"):
+        query = '''
+        SELECT p.id, p.bathroomcnt, p.bedroomcnt, p.calculatedfinishedsquarefeet, p.fips, p.fullbathcnt, p.latitude, p.longitude, p.roomcnt, p.yearbuilt, p.taxvaluedollarcnt, ROUND((p.taxamount / p.taxvaluedollarcnt) * 100, 2) AS taxrate
+        FROM properties_2017 AS p
+        JOIN predictions_2017 AS pr USING (parcelid)
+        WHERE p.propertylandusetypeid IN (261, 262, 263, 264, 266, 268, 273, 276, 279)
+        AND pr.transactiondate BETWEEN "2017-05-01" AND '2017-06-30';
+        '''
+
+        url = get_url('zillow')
+
+        zillow = pd.read_sql(query, url, index_col='id')
+
+        zillow.to_csv('zillow.csv')
+        
     zillow = pd.read_csv('zillow.csv')
-    FIPS = pd.read_csv('fips.csv')
-    zillow = zillow.merge(FIPS, left_on='fips', right_on='FIPS')
+
+    zillow['County'] = zillow.apply(lambda row: label_county(row), axis=1)
+
+    zillow['State'] = 'CA'
+
     return zillow
